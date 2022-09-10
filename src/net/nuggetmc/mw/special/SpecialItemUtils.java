224f913ecfd6c74510a21ffc7ba3f8a72454789e
keaidehuangpi <83892521+keaidehuangpi@users.noreply.github.com>
2022-09-10 22:55:46 +0800

    Cow bucket

--- src/net/nuggetmc/mw/special/SpecialItemUtils.java
+++ src/net/nuggetmc/mw/special/SpecialItemUtils.java
@@ -5,10 +5,15 @@ import net.minecraft.server.v1_8_R3.Item;
 import net.minecraft.server.v1_8_R3.Items;
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import net.nuggetmc.mw.MegaWalls;
+import net.nuggetmc.mw.mwclass.classes.MWCow;
 import net.nuggetmc.mw.mwclass.items.MWPotions;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.entity.Cow;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class SpecialItemUtils {
     private MegaWalls plugin=MegaWalls.getInstance();
@@ -23,6 +28,13 @@ public class SpecialItemUtils {
     }
     public ItemStack getCowBucket(){
         ItemStack milk=new ItemStack(Material.MILK_BUCKET,1);
+        ArrayList<String> lore=new ArrayList<>();
+        lore.add(ChatColor.BLUE+"Resistance I (5s)");
+        lore.add(ChatColor.BLUE+"Regeneration II (5s) "+ChatColor.GRAY+"(2 "+ChatColor.RED+"❤"+ChatColor.RESET+")");
+        ItemMeta itemMeta=milk.getItemMeta();
+        itemMeta.setDisplayName(ChatColor.WHITE+"Ultra Pasteurized Milk Bucket");
+        itemMeta.setLore(lore);
+        milk.setItemMeta(itemMeta);
         net.minecraft.server.v1_8_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(milk);
         NBTTagCompound compound = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
 
@@ -43,6 +55,9 @@ public class SpecialItemUtils {
     }
     public ItemStack getCowOwnBucket(int amount){
         ItemStack milk=new ItemStack(Material.MILK_BUCKET,amount);
+        ItemMeta itemMeta= milk.getItemMeta();
+        itemMeta.setDisplayName(ChatColor.LIGHT_PURPLE+"Cow Buckets Regeneration II (2 Hearts / 0:005s) & Resistance I (0:05s)");
+        milk.setItemMeta(itemMeta);
         net.minecraft.server.v1_8_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(milk);
         NBTTagCompound compound = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
 
