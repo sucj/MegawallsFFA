4a7017868dc83754c2e5bc3c842cfd120b847679
keaidehuangpi <83892521+keaidehuangpi@users.noreply.github.com>
2022-10-07 23:04:32 +0800

    张泽旭我不爱你了

--- src/net/nuggetmc/mw/utils/EsuUtils.java
+++
@@ -1,9 +0,0 @@
-package net.nuggetmc.mw.utils;
-
-public class EsuUtils extends Object{
-    String 我的世界酷黑解说=new String("EN_JIA");
-    public void 张泽旭() {
-        System.out.println(我的世界酷黑解说);
-
-    }
-}
