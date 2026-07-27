6c99335f91e08ddc6cba28b337b61de0a1a0b537
chips <keaidehuangpi@163.com>
2026-07-27 12:57:26 +0800

    don't break barrier!

--- src/net/nuggetmc/mw/special/SpecialEventsManager.java
+++ src/net/nuggetmc/mw/special/SpecialEventsManager.java
@@ -696,6 +696,7 @@ public class SpecialEventsManager implements Listener {
         Block clickedBlock = e.getClickedBlock();
         if (clickedBlock == null || clickedBlock.getType() == Material.AIR) return;
         if (clickedBlock.getType()==Material.BEDROCK) return;
+        if (clickedBlock.getType()==Material.BARRIER) return;
         ((CraftPlayer) p).getHandle().playerInteractManager.breakBlock(
                 new BlockPosition(clickedBlock.getX(), clickedBlock.getY(), clickedBlock.getZ())
         );
